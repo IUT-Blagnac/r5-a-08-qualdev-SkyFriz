b4284211db3fb44a54f82372a6c1925021cc0807
skyfriz <thomastesta@orange.com>
2023-12-13 10:17:13 +0100

    part2 TP2

--- cucumber_demo/src/test/java/dojo/CocktailSteps.java
+++ cucumber_demo/src/test/java/dojo/CocktailSteps.java
@@ -8,6 +8,7 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import dossier.Order;
 
 public class CocktailSteps {
     private Order order;
