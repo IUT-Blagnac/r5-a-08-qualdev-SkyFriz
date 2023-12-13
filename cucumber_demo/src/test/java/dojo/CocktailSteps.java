a0bbc0fbe84f4ff2cc680dcbd3ec3c4aaf8ae8a8
skyfriz <thomastesta@orange.com>
2023-12-13 10:26:37 +0100

    part3 TP2

--- cucumber_demo/src/test/java/dojo/CocktailSteps.java
+++ cucumber_demo/src/test/java/dojo/CocktailSteps.java
@@ -13,20 +13,20 @@ import dossier.Order;
 public class CocktailSteps {
     private Order order;
 
-    @Given("Romeo who wants to buy a drink")
-    public void Romeo_who_wants_to_buy_a_drink() {
+    @Given("{string} who wants to buy a drink")
+    public void Romeo_who_wants_to_buy_a_drink(String x) {
         order = new Order();
-        order.declareOwner("Romeo");
+        order.declareOwner(x);
     }
 
-    @When("an order is declared for Juliette")
-    public void an_order_is_declared_for_Juliette(){
-        order.declareTarget("Juliette");
+    @When("an order is declared for {string}")
+    public void an_order_is_declared_for_Juliette(String y){
+        order.declareTarget(y);
     }
 
     @Then("there is no cocktail in the order")
-    public void there_is_no_cocktail_in_the_order(){
+    public void there_is_no_cocktail_in_the_order(int nmb){
         List<String> cocktails =  order.getCocktails();
-        assertEquals(0, cocktails.size());
+        assertEquals(nmb, cocktails.size());
     }
 }
